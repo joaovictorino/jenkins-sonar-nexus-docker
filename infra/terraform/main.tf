7377ee404b8843be629f9f90849873430328b932
joaovictorino <jhvictorino@gmail.com>
2022-05-20 14:57:43 -0300

    azure principal service

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -12,6 +12,11 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "example" {
